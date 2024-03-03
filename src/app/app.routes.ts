6e6527cb6ef5e120e481ed1fb99db4dc71ddcbc2
GalaktionDanelia <galadanelia1@gmail.com>
2024-03-03 12:08:43 +0400

    add payment page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { CheckReturnComponent } from './components/check-return/check-return.component';
 import { RoutesComponent } from './components/routes/routes.component';
 import { PassengersComponent } from './components/passengers/passengers.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'check-return', component: CheckReturnComponent },
   { path: 'routes', component: RoutesComponent },
   { path: 'passengers', component: PassengersComponent },
+  { path: 'payment', component: PaymentComponent },
 ];
--- src/app/components/passengers/passengers.component.ts
+++ src/app/components/passengers/passengers.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../ui/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-passengers',
@@ -8,4 +9,9 @@ import { ButtonComponent } from '../../ui/button/button.component';
   templateUrl: './passengers.component.html',
   styleUrl: './passengers.component.scss',
 })
-export class PassengersComponent {}
+export class PassengersComponent {
+  constructor(private router: Router) {}
+  onBuyNow() {
+    this.router.navigate(['payment']);
+  }
+}
